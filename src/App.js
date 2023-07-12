c03fcd35897566ed7d8ff413492fd39a052cd8ea
seonghye <tjdgp0817@gmail.com>
2023-07-12 17:05:18 +0900

    백그라운드 이미지 삽입

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ function App() {
           <Navbar.Brand href="#home">SSHOP</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg" style={{ backgroundImage: "url()" }}></div>
     </div>
   );
 }
