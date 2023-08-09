177389500c03b8c7906b0ab2172b3931a320a91e
seonghye <tjdgp0817@gmail.com>
2023-08-09 15:01:07 +0900

    cart 데이터 바인딩

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,6 +1,9 @@
 import { Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function Cart() {
+  let cartItems = useSelector((state) => state.cart); //Redux store 가져와줌
+  console.log(cartItems);
   return (
     <div>
       <Table>
@@ -13,12 +16,15 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+          {cartItems.map((item, index) => (
+            <tr key={item.id}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.count}</td>
+              <td>변경 버튼</td>{" "}
+              {/* 여기에 변경 버튼을 추가하거나 원하는 내용을 삽입하세요 */}
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
