751b7269f8d75bf37ac01bc71830ddddd664c541
seonghye <tjdgp0817@gmail.com>
2023-08-15 15:34:33 +0900

    increase 함수

--- src/store.js
+++ src/store.js
@@ -4,8 +4,8 @@ let user = createSlice({
   name: "user",
   initialState: { name: "kim", age: 20 },
   reducers: {
-    changeName(state) {
-      return { name: "park", age: 20 };
+    increase(state, a) {
+      state.age += a.payload;
     },
   },
 });
